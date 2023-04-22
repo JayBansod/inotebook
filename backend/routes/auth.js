f5a29630c29cae653aa9f8e39d500bfb1014d52e
JayBansod <jaybansod678@gmail.com>
2023-04-23 02:31:34 +0530

    video 46 done user name email password validation is done in file auth.js we import validation by 'npm i --save  express-validator'

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,38 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
+// for validation
+const { body, validationResult } = require("express-validator");
 
 // create a user using : POST "/api/auth/".Dosent require Auth
-router.post("/", (req, res) => {
-  console.log(req.body);
-  const user = User(req.body);
-  res.send(req.body);
-  user.save();
-});
+router.post(
+  "/",
+  [
+    body("name", "enter a valid name").isLength({ min: 3 }),
+    body("email", "enter a valid email").isEmail(),
+    body("password").isLength({ min: 5 }),
+  ],
+  (req, res) => {
+    // for save without validation
+    // console.log(req.body);
+    // const user = User(req.body);
+    // res.send(req.body);
+    // user.save();
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    User.create({
+      name: req.body.name,
+      password: req.body.password,
+      email: req.body.email,
+    })
+      .then((user) => res.json(user))
+      .catch(
+        (err) => console.log(err),
+        res.json({ error: "Enter unique value" })
+      );
+  }
+);
 module.exports = router;
