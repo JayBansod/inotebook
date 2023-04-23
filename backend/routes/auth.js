eca772e1999e6156a967971bcb5562d887fde75f
JayBansod <jaybansod678@gmail.com>
2023-04-23 11:49:24 +0530

    video 47 done some comment are added in auth.js try catch block added to fetch internal error collection are created in thunderclient

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,35 +4,47 @@ const User = require("../models/User");
 // for validation
 const { body, validationResult } = require("express-validator");
 
-// create a user using : POST "/api/auth/".Dosent require Auth
+// create a user using : POST "/api/auth/createuser".no lofin required
 router.post(
-  "/",
+  "/createuser",
   [
     body("name", "enter a valid name").isLength({ min: 3 }),
     body("email", "enter a valid email").isEmail(),
     body("password").isLength({ min: 5 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     // for save without validation
     // console.log(req.body);
     // const user = User(req.body);
     // res.send(req.body);
     // user.save();
 
+    //If there are errors , return bad request and errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
-      name: req.body.name,
-      password: req.body.password,
-      email: req.body.email,
-    })
-      .then((user) => res.json(user))
-      .catch(
-        (err) => console.log(err),
-        res.json({ error: "Enter unique value" })
-      );
+    // check weather the email exist already
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res.status(400).json({ error: "Email exist" });
+      }
+      user = await User.create({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email,
+      });
+      // .then((user) => res.json(user))
+      // .catch(
+      //   (err) => console.log(err),
+      //   res.json({ error: "Enter unique value" })
+      // );
+      res.json({ user });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("some error occur");
+    }
   }
 );
 module.exports = router;
