9479d9babf14b6ee4f64e7e2e0739b6207884879
JayBansod <jaybansod678@gmail.com>
2023-04-28 09:55:55 +0530

    in video 55 we done install the pm i rect-router-dom concurrently for running both the server at a time we are running nodemon and localhost at a time we add script in package.json video 56 done components are added like homee about error

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Homee from "./components/Homee";
+import { Routes, Route } from "react-router-dom";
+import About from "./components/About";
+import Error from "./components/Error";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Homee />} />
+        <Route exact path="/home" element={<Homee />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="*" element={<Error />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/components/Homee.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Homee = () => {
+  return (
+    <>
+      <h1>Home</h1>
+    </>
+  );
+};
+
+export default Homee;
