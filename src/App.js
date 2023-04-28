1ae84a8dccd2853650947affda069a1a69a449f4
JayBansod <jaybansod678@gmail.com>
2023-04-29 01:50:03 +0530

    video 62 done we added the card for notes and two fontaswame link in index.html and use two icon

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import { Routes, Route } from "react-router-dom";
 import About from "./components/About";
 import Error from "./components/Error";
 import NoteState from "./context/notes/NotesState";
+import Alert from "./components/Alert";
 function App() {
   return (
     <>
       <NoteState>
         <Navbar />
+        <Alert message={"ashb"} />
         <div className="container">
           <Routes>
             <Route exact path="/" element={<Homee />} />
--- src/components/Noteitem.js
+++ src/components/Noteitem.js
@@ -8,7 +8,11 @@ const Noteitem = (props) => {
         <div className="card my-3">
           <div className="card-body">
             <h5 className="card-title">{note.title}</h5>
-            <p className="card-text">{note.description}</p>
+            <p className="card-text ">{note.description}</p>
+            <div className="d-flex align-items-center justify-content-between pt-2 my-2">
+              <i className="fa-solid fa-trash fa-xl mx-2"></i>
+              <i className="fa-regular fa-pen-to-square fa-xl mx-2"></i>
+            </div>
           </div>
         </div>
       </div>
