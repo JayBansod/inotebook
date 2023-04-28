0f3b8a086dc5b6450346c460f834ff6a2b831366
JayBansod <jaybansod678@gmail.com>
2023-04-28 16:34:29 +0530

    video 58 done

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ import Homee from "./components/Homee";
 import { Routes, Route } from "react-router-dom";
 import About from "./components/About";
 import Error from "./components/Error";
+import NoteState from "./context/notes/NotesState";
 function App() {
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route exact path="/" element={<Homee />} />
-        <Route exact path="/home" element={<Homee />} />
-        <Route exact path="/about" element={<About />} />
-        <Route exact path="*" element={<Error />} />
-      </Routes>
+      <NoteState>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Homee />} />
+          <Route exact path="/home" element={<Homee />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="*" element={<Error />} />
+        </Routes>
+      </NoteState>
     </>
   );
 }
