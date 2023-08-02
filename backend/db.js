9228c5bd53cad51def6050b101304b07ff93a869
JayBansod <jaybansod678@gmail.com>
2023-08-02 23:39:09 +0530

    db is edited for atlas

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    mongoose.connect("mongodb://localhost:27017/inotebook");
+    // mongoose.connect("mongodb://localhost:27017/inotebook");
+    mongoose.connect(
+      // "mongodb+srv://shopping:Jay123@cluster0.tvr06ed.mongodb.net/shopping?retryWrites=true&w=majority"
+      "mongodb+srv://jaybansod:jaybansod@inotebook.ikyfcev.mongodb.net/?retryWrites=true&w=majority"
+    );
     console.log("Mongo connected");
   } catch (error) {
     console.log(error);
